fa6eb4e49bba37ba0804c1cc3d65f098d6a38340
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-05-31 04:39:33 +0100

    setup map of category array

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 // import './App.css';
 
 const App = () => {
+
+  // data for categories
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Trainers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    }
+  ]
   return (
    <div className="categories-container">
-     <div className="category-container">
-      {/* {<img />} */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-     </div>
-   </div>
-
+    {/* map through each category in the categories array of category objects */}
+    {/* desctructured off directly to obtain the keys from categories */}
+      {categories.map(({title, id}) => (
+        // return the JSX structure of the categories
+          <div key={id} className="category-container">
+            <div className="background-image" />
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+         </div>
+      ))}
+      {/* end categories container */}
+    </div>
   );
 }
 
